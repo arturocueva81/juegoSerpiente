e7e879679305f3bdae2c904c661a5d08dc4520f7
arturocueva81 <ajcs81@hotmail.com>
2026-05-12 17:27:49 -0500

    Ejercicio Final, cabeza amarilla y cuerpo rojo

--- serpiente.js
+++ serpiente.js
@@ -3,6 +3,32 @@ const canvas = document.getElementById("canvasJuego");
 const ctx = canvas.getContext("2d");
 const TAMANIO_CELDA = 25;
 
+
+// const SERPIENTE = [ //ejercicio 2, serpiente
+//   { x: (canvas.width/2)/TAMANIO_CELDA, y: (canvas.height/2)/TAMANIO_CELDA },
+//   {x:((canvas.width/2)/TAMANIO_CELDA)-1, y: ((canvas.height/2)/TAMANIO_CELDA) },
+//   {x: ((canvas.width/2)/TAMANIO_CELDA)-2, y: ((canvas.height/2)/TAMANIO_CELDA) },
+//   {x:((canvas.width/2)/TAMANIO_CELDA), y:((canvas.height/2)/TAMANIO_CELDA)+1 }
+// ];
+
+// const SERPIENTE = [// ejercicio 3, serpiente de 5 cuadros al borde izquierdo
+//   { x: 0, y: 14 },
+//   { x: 0, y: 13 },
+//   { x: 0, y: 12 },
+//   { x: 0, y: 11 },
+//   { x: 0, y: 10 },
+// ];
+
+const SERPIENTE = [ // Ejercicio Final
+  { x: (canvas.width / 2) / TAMANIO_CELDA, y: (canvas.height / 2) / TAMANIO_CELDA },
+  { x: (canvas.width / 2) / TAMANIO_CELDA, y: (canvas.height / 2) / TAMANIO_CELDA - 1 },
+  { x: (canvas.width / 2) / TAMANIO_CELDA - 1, y: (canvas.height / 2) / TAMANIO_CELDA - 1 },
+  { x: (canvas.width / 2) / TAMANIO_CELDA - 2, y: (canvas.height / 2) / TAMANIO_CELDA - 1 },
+];
+
+
+
+
 // Primera pintura del juego al cargar la página
 dibujarTodo();
 
@@ -17,6 +43,16 @@ function limpiarCanvas() {
 function dibujarTodo() {
   limpiarCanvas();
   dibujarTablero();
+  // pintarParte(5, 5);
+  // pintarParte(10, 2);
+  // pintarParte(
+  //   (canvas.height - TAMANIO_CELDA) / TAMANIO_CELDA,
+  //   (canvas.width - TAMANIO_CELDA) / TAMANIO_CELDA,
+  // );
+  // pintarParte((canvas.height - TAMANIO_CELDA) / TAMANIO_CELDA, 10);
+  // pintarParte(0, (canvas.width - TAMANIO_CELDA) / TAMANIO_CELDA);
+  // pintarParte((canvas.height - TAMANIO_CELDA) / TAMANIO_CELDA, 0);
+  pintarSerpiente();
 }
 
 function dibujarTablero() {
@@ -36,3 +72,21 @@ function dibujarTablero() {
   }
 }
 
+function pintarParte(lineaX, lineaY, colorRelleno="red") {
+  let valorX = lineaX * TAMANIO_CELDA;
+  let valorY = lineaY * TAMANIO_CELDA;
+
+  ctx.fillStyle = colorRelleno;
+  ctx.fillRect(valorX, valorY, TAMANIO_CELDA, TAMANIO_CELDA);
+
+  ctx.strokeStyle = "black";
+  ctx.strokeRect(valorX, valorY, TAMANIO_CELDA, TAMANIO_CELDA);
+}
+
+function pintarSerpiente(){
+  for(let i = 0; i < SERPIENTE.length; i++){
+    let elemento = SERPIENTE[i];
+    const color = i === 0 ? "yellow" : "red";
+    pintarParte(elemento.x,elemento.y, color);
+  }
+}
